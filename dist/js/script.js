6bc00a0b3269cca383899363c3dc2ab815f790df
svetlana nemova <photiniya@gmail.com>
2022-11-25 13:12:26 +0300

    A block of skills was created, it was adapted for phones - 320 pixels. Added skill scale script.

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,3 +11,14 @@ clickBurger.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+
+//skills
+
+const skillBlock = document.querySelectorAll('.skills__block');
+
+skillBlock.forEach(function(elem) {
+    let percent = elem.childNodes[1].outerText;
+    let nowSkills = elem.childNodes[2].children[1];
+    nowSkills.style.width = `${percent}`;
+})
